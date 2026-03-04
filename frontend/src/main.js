6f7d4a21dc9b1085e4d41df22994e83496914025
Ussuin <emanuelgzzxd@gmail.com>
2026-03-03 18:15:16 -0600

    Add console logs for API and client IDs
    Log API and client IDs to the console for debugging.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { apiUrl, googleClientId, githubClientId } from "./config";
+
+console.log(apiUrl);
+console.log(googleClientId);
+console.log(githubClientId);
+
 
 createApp(App).mount('#app')
