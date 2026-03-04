6b10a7194807d02cfa9fb933c32085b0ec640015
Ussuin <emanuelgzzxd@gmail.com>
2026-03-03 18:13:53 -0600

    Delete backend/src/config.js

--- backend/src/config.js
+++
@@ -1,3 +0,0 @@
-export const apiUrl = import.meta.env.VITE_API_URL;
-export const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
-export const githubClientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
