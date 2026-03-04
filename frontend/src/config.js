842d50bbdac543360adcdd8254ea1f6c785a4326
Ussuin <emanuelgzzxd@gmail.com>
2026-03-03 18:14:52 -0600

    Add configuration for API and client IDs

---
+++ frontend/src/config.js
@@ -0,0 +1,3 @@
+export const apiUrl = import.meta.env.VITE_API_URL;
+export const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
+export const githubClientId = import.meta.env.VITE_GITHUB_CLIENT_ID;
